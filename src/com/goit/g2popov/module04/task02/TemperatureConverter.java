ab545756cc771a3766f9d3cec1e076b995ff243c
Andrii Popov <andrey.popov85@gmail.com>
2016-05-30 18:12:01 +0300

    Homework task02

---
+++ src/com/goit/g2popov/module04/task02/TemperatureConverter.java
@@ -0,0 +1,14 @@
+package com.goit.g2popov.module04.task02;
+
+/**
+ * Created by Андрей on 30.05.2016.
+ */
+public class TemperatureConverter {
+        public double convertIntoCelsius (double temperatureInFahrenheit) {
+                return (temperatureInFahrenheit-32)/1.8;
+        }
+
+        public double convertIntoFahrenheit (double temperatureInCelsius) {
+                return temperatureInCelsius*9/5+32;
+        }
+}
